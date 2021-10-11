0c289904165e59905c81cb0299e7a69ba1e76b29
Aaron Gillies <aarongillies88@gmail.com>
2021-10-11 19:18:23 -0400

    added dotenv and basic scaffold for main component

--- angular-test/src/app/app.module.ts
+++ angular-test/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MainContainerComponent } from './main-container-component/main-container-component';
+import { MainNavigationComponent } from './main-navigation/main-navigation.component';
 
 @NgModule({
-  declarations: [AppComponent, MainContainerComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, MainContainerComponent, MainNavigationComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- angular-test/src/app/main-container-component/main-container-component.ts
+++ angular-test/src/app/main-container-component/main-container-component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../API/api.service';
 
+interface article {
+  title: string;
+}
 @Component({
   selector: 'app-main-container-component',
   templateUrl: './main-container-component.html',
   styleUrls: ['./main-container-component.css'],
 })
 export class MainContainerComponent implements OnInit {
-  constructor() {}
+  constructor(ApiService: ApiService) {}
 
-  ngOnInit(): void {}
+  articles: Array<article> = [];
+
+  ngOnInit(): void {
+    // Make call with ApiService client and fetch and return some data
+    console.log(this.articles);
+  }
+
+  fetchNewsArticles(): Array<article> {
+    return [];
+  }
 }
