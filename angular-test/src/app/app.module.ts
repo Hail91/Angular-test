bb54d253b3dac177db560b0e9092f66c765b70b8
Aaron Gillies <aarongillies88@gmail.com>
2021-10-12 18:12:05 -0400

    data coming through to client, need to work on formatting and cleaning some stuff up

--- angular-test/src/app/app.module.ts
+++ angular-test/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MainContainerComponent } from './main-container-component/main-container-component';
 import { MainNavigationComponent } from './main-navigation/main-navigation.component';
+import { ArticleCardComponent } from './article-card/article-card.component';
 
 @NgModule({
-  declarations: [AppComponent, MainContainerComponent, MainNavigationComponent],
+  declarations: [AppComponent, MainContainerComponent, MainNavigationComponent, ArticleCardComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ angular-test/src/app/article-card/article-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { article } from '../main-container-component/interfaces/index';
+
+@Component({
+  selector: 'app-article-card',
+  templateUrl: './article-card.component.html',
+  styleUrls: ['./article-card.component.css'],
+})
+export class ArticleCardComponent implements OnInit {
+  @Input() article!: article;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
